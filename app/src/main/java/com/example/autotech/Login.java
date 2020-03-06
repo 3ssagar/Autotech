85b5fbd18e046592c21f9ccf3d5e190eeda072c8
3ssagar <3s.sagar@gmail.com>
2020-03-05 21:29:44 -0500

    added database

--- app/src/main/java/com/example/autotech/Login.java
+++ app/src/main/java/com/example/autotech/Login.java
@@ -1,59 +1,96 @@
 package com.example.autotech;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
-    EditText edit1,edit2;
-    Button b1,b2;
-    String Username,Passcode;
+    EditText mEmail,mPassword;
+    Button mLoginBtn;
+    TextView mCreateBtn;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        edit1= findViewById(R.id.make);
-        edit2= findViewById(R.id.model);
-        b1= findViewById(R.id.button);
-        b2=findViewById(R.id.button3);
 
-        edit1.setText(Username);
-        edit2.setText(Passcode);
-        b1.setOnClickListener(new View.OnClickListener() {
+        mEmail = findViewById(R.id.Email);
+        mPassword = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progressBar);
+        fAuth = FirebaseAuth.getInstance();
+        mLoginBtn = findViewById(R.id.loginBtn);
+        mCreateBtn = findViewById(R.id.createText);
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
 
-                Username = edit1.getText().toString();
-                Passcode = edit2.getText().toString();
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is Required.");
+                    return;
+                }
 
-                Intent i1 = new Intent(Login.this, Signup.class);
-               i1.putExtra("key3", "Username");
-                i1.putExtra("key4", "Passcode");
-                startActivity(i1);
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is Required.");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    mPassword.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                // authenticate the user
+
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), Locations.class));
+                        }else {
+                            Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+
+                    }
+                });
 
             }
         });
 
-        b2.setOnClickListener(new View.OnClickListener() {
+
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Username = edit1.getText().toString();
-                Passcode = edit2.getText().toString();
-
-                Intent i1 = new Intent(Login.this, Locations.class);
-                i1.putExtra("key3", "Username");
-                i1.putExtra("key4", "Passcode");
-                startActivity(i1);
-
+                startActivity(new Intent(getApplicationContext(),Signup.class));
             }
         });
+
 
     }
 }
