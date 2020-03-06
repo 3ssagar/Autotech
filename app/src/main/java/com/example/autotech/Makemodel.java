3ed6c2ba386eaa6d193fae011df957da053cc943
NaramAkhil <naramakhil@gmail.com>
2020-03-06 11:29:12 -0500

    Merge branch 'master' of C:\Users\naram\Downloads\Autotech with conflicts.

--- app/src/main/java/com/example/autotech/Makemodel.java
+++ app/src/main/java/com/example/autotech/Makemodel.java
@@ -1,43 +1,104 @@
 package com.example.autotech;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.ColorSpace;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import java.time.Year;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class Makemodel extends AppCompatActivity {
-    EditText e1,e2,e3;
-    String Make,Model,Year;
-    Button b1;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+
+public class Makemodel extends AppCompatActivity implements View.OnClickListener {
+    EditText nameet,qtyet,pricet;
+    Button postbt,retrievebt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_makemodel);
-        e1 = findViewById(R.id.make);
-        e2 = findViewById(R.id.model);
-        e3 = findViewById(R.id.year);
-        b1= findViewById(R.id.btnsubmit);
-        e1.setText(Make);
-        e2.setText(Model);
-        e3.setText(Year);
-        b1.setOnClickListener(new View.OnClickListener() {
+
+
+        nameet = findViewById(R.id.mk);
+        qtyet = findViewById(R.id.md);
+        pricet = findViewById(R.id.yr);
+        postbt = findViewById(R.id.add);
+        retrievebt = findViewById(R.id.retrive);
+
+        postbt.setOnClickListener(this);
+        retrievebt.setOnClickListener(this);
+        //ALT+ENTER
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.retrive:
+                retrieveDataFromFirestore();
+                break;
+            case R.id.add:
+                addDataIntoFirestore();
+                break;
+        }
+    }
+
+    public void addDataIntoFirestore(){
+        String name = nameet.getText().toString();
+        String qty = qtyet.getText().toString();
+        String price = pricet.getText().toString();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        HashMap<String, Object> order = new HashMap<>();
+        order.put("name", name);
+        order.put("qty", qty);
+        order.put("price", price);
+
+// Add a new document with a generated ID
+        db.collection("Orders")
+                .add(order)
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        Toast.makeText(Makemodel.this,"Added",Toast.LENGTH_LONG).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
             @Override
-            public void onClick(View v) {
-                Make = e1.getText().toString();
-                Model = e2.getText().toString();
-                Year = e3.getText().toString();
-                Intent i1 = new Intent(Makemodel.this, Price.class);
-                i1.putExtra("key5", "Make");
-                i1.putExtra("key6", "Model");
-                i1.putExtra("key7", "Year");
-                startActivity(i1);
+            public void onFailure(@NonNull Exception e) {
+
+                Toast.makeText(Makemodel.this,"Failed",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void retrieveDataFromFirestore(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Orders")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(QueryDocumentSnapshot documentSnapshot:task.getResult()){
+                                System.out.println(documentSnapshot.getData().toString());
+                            }
+                        }
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
 
             }
         });
